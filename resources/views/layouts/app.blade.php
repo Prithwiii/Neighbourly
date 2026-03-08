be13f508e335c816a1885b0bffdc1ce0e623f499
Rafee Uddin <rafeeuddin88@gmail.com>
2026-03-08 19:40:47 +0600

    added a welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         @auth
             <a href="{{ route('dashboard') }}">Dashboard</a> |
             <a href="{{ route('lost-items.index') }}">Lost & Found</a> |
+            {{-- <a href="{{ route('lost-items.index') }}">Announcements</a> | --}}
             <form method="POST" action="{{ route('logout') }}" style="display:inline;">
                 @csrf
                 <button type="submit">Logout</button>
