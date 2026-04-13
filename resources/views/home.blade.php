a12418923623895ed2fc1dfb182a1e431857ad0b
Raisa Raihan Prithwi <raisa.raihan.prithwi@g.bracu.ac.bd>
2026-04-13 15:02:30 +0600

    fix random lines

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,19 +59,18 @@
                 Map
             </a>
 
-<<<<<<< HEAD
             <a href="{{ route('news.index') }}"
                class="p-5 rounded-2xl bg-white/30 hover:bg-white/40
                       backdrop-blur-md border border-white/30
                       shadow-md hover:shadow-xl transition text-center text-emerland">
                 News
-=======
+            </a>
+            
             <a href="{{ route('services.index') }}"
                class="p-5 rounded-2xl bg-white/30 hover:bg-white/40
                       backdrop-blur-md border border-white/30
                       shadow-md hover:shadow-xl transition text-center text-emerald-900">
                 Local Services
->>>>>>> e20a5efc7ef416956768205d8a5fb77ba9712b33
             </a>
 
         </div>
