c942a8568dd90169fa2e45d117dc934f7441087c
Rafee Uddin <rafeeuddin88@gmail.com>
2026-04-20 03:31:54 +0600

    fixed merge conflict prithwi-all and working-version-1 5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,16 +96,6 @@
                 Local Services
             </a>
 
-<<<<<<< HEAD
-=======
-            <a href="{{ route('posts.hub') }}"
-               class="p-5 rounded-2xl bg-white/30 hover:bg-white/40
-                      backdrop-blur-md border border-white/30
-                      shadow-md hover:shadow-xl transition text-center text-emerald-900">
-                Community Blog
-            </a>
-
->>>>>>> 0f904aed0f51464e6dc8a420db3df9a48e7570a7
                 <a href="{{ route('emergency-alerts.index') }}"
                     class="p-5 rounded-2xl bg-white/30 hover:bg-white/40
                              backdrop-blur-md border border-white/30
@@ -113,7 +103,6 @@
                      Emergency Alerts
                 </a>
             
-<<<<<<< HEAD
             <a href="{{ route('posts.hub') }}"
                class="p-5 rounded-2xl bg-white/30 hover:bg-white/40
                       backdrop-blur-md border border-white/30
@@ -121,8 +110,6 @@
                 Community Blog
             </a>
 
-=======
->>>>>>> 0f904aed0f51464e6dc8a420db3df9a48e7570a7
             <a href="{{ route('news.index') }}"
                class="p-5 rounded-2xl bg-white/30 hover:bg-white/40
                       backdrop-blur-md border border-white/30
